08f9d03672efbc15f6f34de48dc0bcdec5fc7033
FaizError <1113769147@qq.com>
2026-05-09 12:18:35 +0800

    添加leetCode1

---
+++ src/main/java/com/hzx/leetcode/leetCode1.java
@@ -0,0 +1,19 @@
+package com.hzx.leetcode;
+
+public class leetCode1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if(nums[left] + nums[right] == target){
+                arr[0] = left;
+                arr[1] = right;
+                break;
+            }
+            left++;
+        }
+        return arr;
+    }
+}
